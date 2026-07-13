71c0a75e4f6c7b8dbcdd0d0928c8cb02985fc7cd
Artem <artem@benzin.local>
2026-07-14 00:21:24 +0300

    fix: импорт CommandObject из aiogram.filters

--- bot/handlers.py
+++ bot/handlers.py
@@ -11,7 +11,7 @@ from datetime import datetime
 from pathlib import Path
 
 from aiogram import Dispatcher, F, Bot
-from aiogram.filters import BaseFilter, Command, CommandStart, StateFilter
+from aiogram.filters import BaseFilter, Command, CommandObject, CommandStart, StateFilter
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import (
